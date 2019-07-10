f85d4b480ced0451bf56dd33885932e41d5717aa
ewe888 <823925783@qq.com>
2019-07-10 09:31:15 +0800

    第二屏

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ $(function () {
         sectionsColor:["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
         verticalCentered:false,
         navigation:true,
-        navigationPosition: "right"
+        navigationPosition: "right",
+        afterLoad:function (link, index) {
+            /*index 序号 1开始  当前屏的序号*/
+            $('.section').eq(index-1).addClass('now');
+            // console.log("idnex"+index);
+        },
     });
 });
